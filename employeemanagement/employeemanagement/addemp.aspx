7c51269851a6c65d21568c8dbdd91f641bc9d889
Syeda Fasiha Faryal <sfasihafaryal@yahoo.com>
2016-04-12 12:02:55 +0500

    ADDED crud ui with db

---
+++ employeemanagement/employeemanagement/addemp.aspx
@@ -0,0 +1,47 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="addemp.aspx.cs" Inherits="employeemanagement.addemp1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        Initials<br />
+        <asp:TextBox ID="txt_initials" runat="server"></asp:TextBox>
+        <br />
+        Surname<br />
+        <asp:TextBox ID="txt_surname" runat="server"></asp:TextBox>
+        <br />
+        Name<br />
+        <asp:TextBox ID="txt_name" runat="server"></asp:TextBox>
+        <br />
+        Mobile<br />
+        <asp:TextBox ID="txt_mobile" runat="server"></asp:TextBox>
+        <br />
+        Current Address<br />
+        <asp:TextBox ID="txt_curraddress" runat="server"></asp:TextBox>
+        <br />
+        Current Designation<br />
+        <asp:TextBox ID="txt_currdesignation" runat="server"></asp:TextBox>
+        <br />
+        Current Salary Package<br />
+        <asp:TextBox ID="txt_currsalary" runat="server"></asp:TextBox>
+        <br />
+        Department<br />
+        <asp:TextBox ID="txt_department" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        
+    <asp:Button ID="Button1" runat="server"  Text="Add" />
+        
+        
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ employeemanagement/employeemanagement/addemp.aspx.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace employeemanagement
+{
+    public partial class addemp1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ////create DBContext object
+            //using (var context = new University_AdministrationEntities1())
+            //{
+            //    var empList = context.employees.ToList<employee>();
+
+            //    //Add student in list
+            //    empList.Add(new employee()
+            //    {
+            //        initials = "Mrertfghf",
+            //        surname = "Afertealana",
+            //        name = "Alpha",
+            //        mobile = "98778678",
+            //        curraddress = "aghfggsfgdfgdfghaf",
+            //        currdesignation = "ederyfgfhf",
+            //        currsalarypkg = 30000,
+            //        department = "Mainhdfhe"
+
+            //    });
+            //    context.SaveChanges();
+
+
+
+
+
+
+
+
+
+
+
+            }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string con = "Data Source =.; Initial Catalog = \"University Administration\"; Integrated Security = True; MultipleActiveResultSets = True; Application Name = EntityFramework";
+            SqlConnection db = new SqlConnection(con);
+            db.Open();
+            string insert = "insert into employee(initials,surname,name,mobile,curraddress,currdesignation,currsalarypkg,department) values ('" + txt_initials.Text + "', '" + txt_surname.Text + "','" + txt_name.Text + "','" + txt_mobile.Text + "','" + txt_curraddress.Text + "','" + txt_currdesignation.Text + "', '" + Convert.ToInt32(this.txt_currsalary.Text) + "' ,'" + txt_department.Text + "')";
+            SqlCommand cmd = new SqlCommand(insert, db);
+            int m = cmd.ExecuteNonQuery();
+            if (m != 0)
+                {
+                Response.Write("< script > alert('Data Inserted !!') </ script >    ");
+
+
+
+            }
+            else   
+{
+                Response.Write("< script > alert('Data Inserted !!') </ script > ");   
+                }
+            db.Close();
+
+        }
+
+
+    }
+
+       
+    }
+
+    
